02b7c4916cdb47a663ef0c511dabfd272d19925c
Emmanuel <pickappgames@gmail.com>
2018-01-04 20:27:04 -0300

    First commit

---
+++ brainer/Assets/Editor/Brainer/ResultGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace Editor.Brainer
+{
+    public class ResultGeneratorTest {
+
+        [Test]
+        public void first_test()
+        {
+            Substitute.For<IMyAction>();
+        }
+        
+    }
+
+    public interface IMyAction
+    {
+    }
+}
