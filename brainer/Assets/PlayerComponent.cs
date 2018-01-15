1c81c5008e7a180c79b7642be19959b4c78d7126
Emmanuel <pickappgames@gmail.com>
2018-01-15 20:44:22 -0300

    Added component to player. Added trigger to collider of results.

---
+++ brainer/Assets/PlayerComponent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerComponent : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		var text = other.gameObject.GetComponent<Text>.text;
+	}
+}
